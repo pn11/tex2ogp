7fc3e1de927409672c04f6b073bbf66187a425e4
oka <pn11@users.noreply.github.com>
2020-09-11 22:19:26 +0900

    add twitter button, add link to title

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h2>TeX2OGP</h2>
+    <h2><a href="/">TeX2OGP</a></h2>
     <router-view/>
     <br>
     <i class="fab fa-github"></i> <a href="https://github.com/pn11/tex2ogp">Source code</a>
@@ -22,5 +22,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#app a:link {
+  color: #2c3e50;
+  text-decoration: none;
+}
+#app a:visited {
+  color: #2c3e50;
+  text-decoration: none;
 }
 </style>
